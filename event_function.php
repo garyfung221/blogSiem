f12e04bc5c8c99952ccb926f766971cbeba52cb3
garyfung221 <garyfung266@gmail.com>
2023-02-28 02:32:08 +0800

    Update event_function.php

--- event_function.php
+++ event_function.php
@@ -1,41 +1,34 @@
 <?php
 include "../includes/logs_db.php";
 
-function login_check_event($username,$password){
+function login_check_event($username, $password){
 
     global $con;
 
-$feature_path = "login";
+    $feature_path = "login";
 
-if (!empty($_SERVER["HTTP_CLIENT_IP"])){
-    $user_ip_address = $_SERVER["HTTP_CLIENT_IP"];
-}elseif(!empty($_SERVER["HTTP_X_FORWARDED_FOR"])){
-    $user_ip_address = $_SERVER["HTTP_X_FORWARDED_FOR"];
-}else{
-    $user_ip_address = $_SERVER["REMOTE_ADDR"];
+    // Get the user's IP address
+    $user_ip_address = isset($_SERVER["HTTP_CLIENT_IP"]) ? $_SERVER["HTTP_CLIENT_IP"] : (isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : $_SERVER["REMOTE_ADDR"]);
+
+    // Check for XSS attack
+    if(preg_match('/<script\b[^>]*>(.*?)<\/script>/is', $username) || preg_match('/<script\b[^>]*>(.*?)<\/script>/is', $password)){
+
+        $action = "xss";
+        $status = "close";
+
+    }
+    // Check for SQL injection attack
+    else if(preg_match('/[\/\';:,"><()|&*]/', $username) || preg_match('/[\/\';:,"><()|&*]/', $password) || $username=="' or 1=1#--"){
+
+        $action = "injection";
+        $status = "close";
+
+    }else{
+        $action = "normal";
+        $status = "open";
+    }
+
+    $query = "INSERT INTO event_logs (event_ip,status,feature_path,event_action,created) VALUES ('$user_ip_address','$status','$feature_path','$action',now())";
+    $event_query = mysqli_query($con,$query);
+    return;
 }
-
-
-
-if(($username=='<script>')||($password=='<script>')){
-
-    $action = "xss";
-    $status = "close";
-
- 
-    
-}else if(($username=="/")||($username==',')||($username=='\'')||($username=='"')||($username==')')||($username=='(')||($username==';')||($password=='/')||($password==',')||($password=='\'')||($password=='"')||($password==')')||($password=='(')||($password==';')||($username=="' or 1=1#--")){
-
-    $action = "injection";
-    $status = "close";
-
-}else{
-    $action = "normal";
-    $status = "open";
-}
-
-$query = "INSERT INTO event_logs (event_ip,status,feature_path,event_action,created) VALUES ('$user_ip_address','$status','$feature_path','$action',now())";
-$event_query = mysqli_query($con,$query);
-return;
-}
-
